ec484bd387b666cb09eeb8ad231bb4c7fd310383
ankurkhurpadi <khurpadiankur@gmail.com>
2026-09-08 19:27:00 +0530

    Initial commit - AWS cloud portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+
+    const message = document.getElementById("message");
+
+    message.textContent =
+        "Thanks for visiting my portfolio! You can connect with me soon.";
+
+}
